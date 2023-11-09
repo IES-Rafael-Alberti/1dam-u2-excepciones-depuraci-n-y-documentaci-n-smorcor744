ae43a101a77be5f2a6381f590f5adf877b3f875f
smorcor744 <mcsergio@iesrafaelalberti.es>
2023-11-09 21:17:11 +0100

    update

--- src/ejer2_3_5.py
+++ src/ejer2_3_5.py
@@ -3,14 +3,16 @@ def contraseña():
     pas = input("Introduzca la contraseña: ")
     if pas != pasware:
         raise NameError("Incorrect Password!!")
-    
+    else:
+        print("Contraseña correcta")
+
+def main():
     try:
         contraseña()
     except NameError as e:
-        print("error" + e)
+        print("**Error** " + str(e))
 
 
-contraseña()
-
-
+if __name__ == "__main__":
+    main()
 
