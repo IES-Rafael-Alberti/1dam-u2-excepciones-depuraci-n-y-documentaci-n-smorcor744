17ae5e9175505d7d670b800af6663b02452867e6
smorcor744 <mcsergio@iesrafaelalberti.es>
2023-11-12 14:25:50 +0100

    update

--- src/ejer2_4.py
+++ src/ejer2_4.py
@@ -1,4 +1,11 @@
 def atgoritmo_burbuja():
+    """
+    Esta función implementa el algoritmo de ordenamiento de burbuja.
+    Ordena una lista de números en orden ascendente.
+
+    Returns:
+        list: La lista de números ordenada en orden ascendente.
+    """
     a = [8, 3, 1, 19, 14]
     total = len(a)
     for i in range(0,len(a)):
@@ -10,11 +17,17 @@ def atgoritmo_burbuja():
 
 
 def main():
+    """
+    Esta es la función principal que se ejecuta cuando se inicia el programa.
+    Llama a la función atgoritmo_burbuja y muestra el resultado.
+    """
     a = atgoritmo_burbuja()
     print(a)
 
 
 if __name__ == "__main__":
+    """
+    Este es el punto de entrada del programa.
+    Llama a la función main cuando se ejecuta el script.
+    """
     main()
-
-
